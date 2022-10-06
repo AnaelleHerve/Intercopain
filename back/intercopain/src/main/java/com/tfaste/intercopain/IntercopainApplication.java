e699459a95d2c8031a2975888c01533acb35e815
Anaëlle HERVE <aherve@consultants-solutec.fr>
2022-10-06 14:39:22 +0200

    adding front & back

---
+++ back/intercopain/src/main/java/com/tfaste/intercopain/IntercopainApplication.java
@@ -0,0 +1,13 @@
+package com.tfaste.intercopain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntercopainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntercopainApplication.class, args);
+	}
+
+}
---
+++ back/intercopain/src/test/java/com/tfaste/intercopain/IntercopainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfaste.intercopain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntercopainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
